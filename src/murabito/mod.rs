b9da8b665d15b2203cdd906ed0357900fa1eb59b
poobis <118197491+poobise@users.noreply.github.com>
2026-04-18 16:22:11 -0500

    test with marker file

--- src/murabito/mod.rs
+++ src/murabito/mod.rs
@@ -3,7 +3,8 @@ mod acmd;
 
 
 pub fn install() {
-    let agent = &mut smashline::Agent::new("murabito");
+    let agent = &mut smashline::Agent::new("murabito")
+    .set_costume(get_marked_costumes("murabito","murabito"));
     acmd::install(agent);
 
     agent.install();
